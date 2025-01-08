aab8916f8a1e10d5800cf14e075a3929aebeffbb
AvivBuzaglo <avivbuzaglo99@gmail.com>
2025-01-08 20:00:46 +0200

    Add basic layout to home page

--- frontend-react/src/pages/HomePage.jsx
+++ frontend-react/src/pages/HomePage.jsx
@@ -2,7 +2,34 @@ export function HomePage() {
     return (
         <section>
             <div className="search-container">
-                <img src="/img/Home-page green.jpg" alt="" className="green-background-img"/>
+                <img src="/img/Home-page-green.jpg" alt="" className="green-background-img"/>
+            </div>
+
+            <div className="categories-container">
+                <button className="categories-btn prog&tech">Programing & Tech</button>
+                <button className="categories-btn graph&design">Graphics & Design</button>
+                <button className="categories-btn marketing">Digital Marketing</button>
+                <button className="categories-btn writing&trans">Writing & Translation</button>
+                <button className="categories-btn video&animation">Video & Animation</button>
+                <button className="categories-btn ai">AI Services</button>
+                <button className="categories-btn music">Music & Audio</button>
+                <button className="categories-btn business">Business</button>
+                <button className="categories-btn consulting">Consulting</button>
+            </div>
+
+                <h2>Made on Fivver</h2>
+            <div className="made-on-container">
+                <img src="/img/made-on.jpg" alt="" />
+                <img src="/img/made-on-1.jpg" alt="" />
+                <img src="/img/made-on-2.jpg" alt="" />
+                <img src="/img/made-on-3.jpg" alt="" />
+                <img src="/img/made-on-4.jpg" alt="" />
+                <img src="/img/made-on-5.jpg" alt="" />
+                <img src="/img/made-on-6.jpg" alt="" />
+                <img src="/img/made-on-7.jpg" alt="" />
+                <img src="/img/made-on-8.jpg" alt="" />
+                <img src="/img/made-on-9.jpg" alt="" />
+                <img src="/img/made-on-10.jpg" alt="" />
             </div>
         </section >
     )
